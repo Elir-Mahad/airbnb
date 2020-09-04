66ac4e748f3833d8247ec3d6313148749da32908
code <mmahad280@gmail.com>
2020-09-03 20:09:42 -0400

    added styles for the cards which will contain rental item data and also we are now displaying data on the page which is hard coded into the home component via props

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Card.css";
 
-function Card() {
-	return <div className="card"></div>;
+function Card({ src, title, description, price }) {
+	return (
+		<div className="card">
+			<img src={src} alt="" />
+			<div className="card_info">
+				<h2>{title}</h2>
+				<h4>{description}</h4>
+				<h3>{price}</h3>
+			</div>
+		</div>
+	);
 }
 
 export default Card;
--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.css";
 import Header from "./Header";
 import Banner from "./Banner";
+import Card from "./Card.js";
 
 function Home() {
 	return (
@@ -9,14 +10,21 @@ function Home() {
 			<Header />
 			<Banner />
 			<div className="home_section">
-				<Card />
-				<Card />
-				<Card />
-			</div>
-			<div className="home_section">
-				<Card />
-				<Card />
-				<Card />
+				<Card
+					src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
+					title="Online Experiences"
+					description="Unique activities we can do together, led by a world of hosts"
+				/>
+				<Card
+					src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
+					title="Entire Homes"
+					description="Comforable private places, with room for friends or family"
+				/>
+				<Card
+					src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
+					title="Online Experiences"
+					description="Unique activities we can do together, led by a world of hosts"
+				/>
 			</div>
 		</div>
 	);
