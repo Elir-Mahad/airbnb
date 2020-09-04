876809e2f4f2805a7a4b03ecebcdd7f679cc7b56
code <mmahad280@gmail.com>
2020-09-03 22:09:52 -0400

    added material ui buttons to searchPage component and also added some p tags to this component

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,11 +1,21 @@
 import React from "react";
 
+import { Button } from "@material-ui/core";
+
 import "./SearchPage.css";
 
 function SearchPage() {
 	return (
 		<div className="searchPage">
-			<h1>Search page</h1>
+			<div className="searchPage_info">
+				<p> 62 stays . 26 august to 30 august . 2 guests </p>
+				<h1> Stays nearby </h1>
+				<Button variant="outlined">Cancellation Flexibility</Button>
+				<Button variant="outlined">Type of place</Button>
+				<Button variant="outlined">Price</Button>
+				<Button variant="outlined">Rooms and beds</Button>
+				<Button variant="outlined">More filters</Button>
+			</div>
 		</div>
 	);
 }
