7fa6a0207b8fd818bbf9576358c06ac7ee30b4b6
code <mmahad280@gmail.com>
2020-09-03 23:36:16 -0400

    added router linking functionality to the header. When you click on the airbnb logo, the page will be re routed to home page.

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 import SearchIcon from "@material-ui/icons/Search";
 import LanguageIcon from "@material-ui/icons/Language";
@@ -9,11 +10,13 @@ import { Avatar } from "@material-ui/core";
 function Header() {
 	return (
 		<div className="header">
-			<img
-				className="header_icon"
-				src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
-				alt=""
-			/>
+			<Link to="/">
+				<img
+					className="header_icon"
+					src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+					alt=""
+				/>
+			</Link>
 
 			<div className="header_center">
 				<input type="text" />
